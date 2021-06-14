0149f89a80e6d25f6aee0daf7f5360cb3a5ba344
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-06-14 20:04:17 +0200

    Fixed html slot in form component

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,13 +1,19 @@
+@php
+    $method = $method ?? 'get';
+@endphp
+
 <form
     class="{{ $class ?? ''}}"
     action="{{ $to }}"
     method="{{ $method === 'get' ? 'get' : 'post' }}"
     @if($allowFile ?? false) enctype="multipart/form-data" @endif
 >
-    @if( $method !== 'get' ) @csrf @endif
-    @if( $method !== 'get' ) @method($method) @endif
+    @if( $method !== 'get' )
+        @csrf
+        @method($method)
+    @endif
     @foreach(($parameters ?? []) as $key => $value)
         <input type="hidden" name="{{ $key }}" value="{{ $value }}" />
     @endforeach
-    {{ $slot ?? '' }}
+    {!! $slot ?? '' !!}
 </form>
