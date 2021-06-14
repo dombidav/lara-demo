dec9ec48adc661a76e65593bcb0fe35918415558
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-06-14 20:02:10 +0200

    Fixed automatic method on magic button

--- resources/views/components/button/magic.blade.php
+++ resources/views/components/button/magic.blade.php
@@ -1,10 +1,16 @@
-<form class="d-inline" action="{{ $route }}" method="post" @isset($confirm) onsubmit="return confirm('{{ $confirm }}')" @endisset>
-@csrf
-@method($method)
+@php
+  $method = $method ?? 'get';
+@endphp
+
+<form class="d-inline" action="{{ $route }}" method="{{ $method === 'get' ? 'get' : 'post' }}" @isset($confirm) onsubmit="return confirm('{{ $confirm }}')" @endisset>
+    @if ($method !== 'get')
+        @csrf
+        @method($method)
+    @endif
     @isset($parameters)
         @foreach($parameters as $key => $value)
             <input type="hidden" name="{{ $key }}" value="{{ $value }}">
         @endforeach
     @endisset
-<input type="submit" class="{{ $class ?? '' }}" value="{{ $slot ?? '' }}">
+<input type="submit" class="btn {{ $class ?? '' }}" value="{{ $slot ?? '' }}">
 </form>
